f48237ca43eae0dca3a6b3b3a87caebb8a93cdb3
Roberto Delgado <216353140+delgadoroberto@users.noreply.github.com>
2026-05-14 22:26:59 -0300

    Initial vulnerable app

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import subprocess
+
+app = Flask(__name__)
+
+PASSWORD = "SuperSecret123"
+
+@app.route("/")
+def home():
+    return "Vulnerable App"
+
+@app.route("/ping")
+def ping():
+    host = request.args.get("host")
+    result = subprocess.check_output(f"ping -c 1 {host}", shell=True)
+    return result
+
+@app.route("/eval")
+def dangerous():
+    data = request.args.get("data")
+    return str(eval(data))
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
